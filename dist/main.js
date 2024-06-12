35bbaccc3d0396d6d940705168ba006e3aa1ffee
atammara <mtasawor68@gamil.com>
2024-06-12 10:14:51 -0700

    commit to main

--- dist/main.js
+++ dist/main.js
@@ -1,30 +1,40 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 import fs from "fs/promises";
-let data = await fs.readFile("data.json", "utf-8");
-const questionsObject = JSON.parse(data);
-let loop = 1;
-let questionsArr = questionsObject.map((item) => {
-    let single_question = {
-        message: item.question,
-        type: "list",
-        choices: item.choices,
-        name: "ques_" + loop,
-    };
-    loop++;
-    return single_question;
-});
-console.log("\n~Welcome to TypeScript Quiz!!~\n");
-let answers = await inquirer.prompt(questionsArr);
-let answersArr = Object.values(answers);
-questionsObject.forEach((item, index) => {
-    if (answersArr[index] == item.answer) {
-        console.log(`\nQuestion # ${index + 1}: ${item.question}`);
-        console.log(`Your answer is Correct: ${item.answer}`);
-    }
-    else {
-        console.log(`\nQuestion # ${index + 1}: ${item.question}`);
-        console.log(`Your answer is wrong, Correct answer is :  ${item.answer}`);
-        console.log(`Your answer was: ${answersArr[index]}`);
-    }
+import path from "path";
+import { fileURLToPath } from "url";
+async function main() {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const dataPath = path.resolve(__dirname, "../data.json");
+    const data = await fs.readFile(dataPath, "utf-8");
+    const questionObject = JSON.parse(data);
+    let loop = 1;
+    const questionArr = questionObject.map((item) => {
+        let single_question = {
+            message: item.question,
+            type: "list",
+            choices: item.choices,
+            name: "ques_" + loop,
+        };
+        loop++;
+        return single_question;
+    });
+    console.log("Welcome to TypeScript Quiz!!");
+    const answers = await inquirer.prompt(questionArr);
+    const answersArr = Object.values(answers);
+    questionObject.forEach((item, index) => {
+        if (answersArr[index] === item.answer) {
+            console.log(`\nQuestion #${index + 1}: ${item.question}`);
+            console.log(`Your answer is Correct: ${item.answer}`);
+        }
+        else {
+            console.log(`\nQuestion #${index + 1}: ${item.question}`);
+            console.log(`Your answer is wrong, Correct answer is: ${item.answer}`);
+            console.log(`Your answer was: ${answersArr[index]}`);
+        }
+    });
+}
+main().catch((err) => {
+    console.error("Error running the quiz:", err);
 });
--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,48 @@
 #! /usr/bin/env node
 
-import inquirer, {Answers} from "inquirer";
+import inquirer, { Answers } from "inquirer";
 import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
 
-let data: string = await fs.readFile("data.json","utf-8");
-const questionsObject : any[] = JSON.parse(data);
+async function main() {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const dataPath = path.resolve(__dirname, "../data.json"); // Path to data.json
 
-let loop : number = 1;
+    const data: string = await fs.readFile(dataPath, "utf-8");
+    const questionObject: any[] = JSON.parse(data);
 
-let questionsArr : Answers[] = questionsObject.map((item) => {
-    let single_question : Answers = {
-        message : item.question,
-        type: "list",
-        choices : item.choices,
-        name: "ques_" + loop,
-    } 
-    loop++;
-    return single_question;
+    let loop: number = 1;
+
+    const questionArr: Answers[] = questionObject.map((item) => {
+        let single_question: Answers = {
+            message: item.question,
+            type: "list",
+            choices: item.choices,
+            name: "ques_" + loop,
+        };
+        loop++;
+        return single_question;
+    });
+
+    console.log("Welcome to TypeScript Quiz!!");
+
+    const answers = await inquirer.prompt(questionArr);
+    const answersArr = Object.values(answers);
+
+    questionObject.forEach((item, index) => {
+        if (answersArr[index] === item.answer) {
+            console.log(`\nQuestion #${index + 1}: ${item.question}`);
+            console.log(`Your answer is Correct: ${item.answer}`);
+        } else {
+            console.log(`\nQuestion #${index + 1}: ${item.question}`);
+            console.log(`Your answer is wrong, Correct answer is: ${item.answer}`);
+            console.log(`Your answer was: ${answersArr[index]}`);
+        }
+    });
+}
+
+main().catch((err) => {
+    console.error("Error running the quiz:", err);
 });
-
-console.log("\n~Welcome to TypeScript Quiz!!~\n");
-
-let answers = await inquirer.prompt(questionsArr);
-
-let answersArr = Object.values(answers);
-
-questionsObject.forEach((item, index) =>{
-    if(answersArr[index] == item.answer) {
-        console.log(`\nQuestion # ${index + 1}: ${item.question}`);
-        console.log(`Your answer is Correct: ${item.answer}`);
-    } else {
-        console.log(`\nQuestion # ${index + 1}: ${item.question}`);
-        console.log(`Your answer is wrong, Correct answer is :  ${item.answer}`);
-        console.log(`Your answer was: ${answersArr[index]}`);
-    }
-})
